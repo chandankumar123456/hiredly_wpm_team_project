92d44b9b1b90589c9562d3b2c00d81bce79cb565
Chandan Kumar <gangaputrachandan@gmail.com>
2024-08-21 06:32:34 +0530

    changed some paths

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -3,10 +3,14 @@ const router = express.Router();
 const ctrlLocations =
 require('../controllers/locations');
 const ctrlOthers = require('../controllers/others');
+const getJobs = require('../controllers/jobs');
 /* Locations pages */
 router.get('/', ctrlLocations.homelist);
 router.get('/location', ctrlLocations.locationInfo);
 router.get('/location/review/new', ctrlLocations.addReview);
+router.get('/jobs/Google_SDE', getJobs.Google);
+router.get('/jobs/Microsoft_DL_Intern', getJobs.Microsoft);
+router.get('/jobs/Amazon_GenAI', getJobs.Amazon)
 /* Other pages */
 router.get('/about', ctrlOthers.about);
 module.exports = router;
