385ca439f72fb984dbe57ec571e70dee6cf8568a
Chandan Kumar <gangaputrachandan@gmail.com>
2024-09-24 22:10:13 +0530

    chaged project added files

--- app.js
+++ app.js
@@ -1,41 +1,34 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+require('dotenv').config(); // Load environment variables from .env file
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const jobRoutes = require('./routes/jobs');
+const aboutRoutes = require('./routes/about');
+const reviewRoutes = require('./routes/reviews');
 
-var indexRouter = require('./app_server/routes/index');
-var usersRouter = require('./app_server/routes/users');
+const app = express();
 
-var app = express();
+// Middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public')); // Serve static files from the public directory
+app.set('view engine', 'ejs');
 
-// view engine setup
-app.set('views', path.join(__dirname, 'app_server', 'views'));
-app.set('view engine', 'jade');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+// Homepage Route
+app.get('/', (req, res) => {
+    res.render('index'); // Render index.ejs for the homepage
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected...'))
+  .catch(err => console.error('MongoDB connection error:', err));
+  
+app.use('/', reviewRoutes);
+// Use routes
+app.use('/jobs', jobRoutes);
+app.use('/', aboutRoutes); // Include the about routes
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+// Start server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
-
-module.exports = app;
